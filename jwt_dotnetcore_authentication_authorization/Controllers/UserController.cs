0d009c266100f4b6026cdb7524810288477817c7
AshiqPranto <ashiqpranto@gmail.com>
2023-05-30 20:40:10 +0600

    template for authentication and authorization done

---
+++ jwt_dotnetcore_authentication_authorization/Controllers/UserController.cs
@@ -0,0 +1,70 @@
+﻿using jwt_dotnetcore_authentication_authorization.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace jwt_dotnetcore_authentication_authorization.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet("Public")]
+        public IActionResult Public()
+        //public ActionResult<string> Public()  //equivalent of IActionResult
+        {
+            return Ok("Hi, you're on public property");
+        }
+
+
+        [HttpGet("Admins")]
+        [Authorize(Roles = "Administrator")]
+        public IActionResult AdminsEndpoint()
+        {
+            var currentUser = GetCurrentUser();
+
+            return Ok($"Hi {currentUser.GivenName}, you are an {currentUser.Role}");
+        }
+        private UserModel GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+
+                return new UserModel
+                {
+                    Username = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value,
+                    EmailAddress = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Email)?.Value,
+                    GivenName = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.GivenName)?.Value,
+                    Surname = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Surname)?.Value,
+                    Role = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Role)?.Value
+                };
+            }
+            return null;
+        }
+
+
+        [HttpGet("Sellers")]
+        [Authorize(Roles = "Seller")]
+        public IActionResult SellersEndpoint()
+        {
+            var currentUser = GetCurrentUser();
+
+            return Ok($"Hi {currentUser.GivenName}, you are a {currentUser.Role}");
+        }
+
+        [HttpGet("AdminsAndSellers")]
+        [Authorize(Roles = "Administrator,Seller")]
+        public IActionResult AdminsAndSellersEndpoint()
+        {
+            var currentUser = GetCurrentUser();
+
+            return Ok($"Hi {currentUser.GivenName}, you are an {currentUser.Role}");
+        }
+
+
+    }
+}
---
+++ jwt_dotnetcore_authentication_authorization/Models/UserLogin.cs
@@ -0,0 +1,8 @@
+﻿namespace jwt_dotnetcore_authentication_authorization.Models
+{
+    public class UserLogin
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
